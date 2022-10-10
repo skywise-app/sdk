b078530ba2cb290d7496a9462ea3e89329930031
John Long <kiino@qq.com>
2022-10-09 17:45:31 -0700

    0.2.2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { customAxios as axios } from './axios';
 export { datadogFactory } from './datadog';
 export { kvdb } from './kvdb';
+export * as ratingSvc from './rating';
+export * as commentSvc from './comment';
