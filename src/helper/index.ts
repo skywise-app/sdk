3d37efe6a107be2ac36f260629a69c021fdfb2db
John Long <kiino@qq.com>
2023-04-22 00:59:43 -0700

    0.3.5

---
+++ src/helper/index.ts
@@ -0,0 +1,5 @@
+// "12345678".split("").sort(()=>Math.random()-0.5).join("")
+export const codes: Record<number, number[]> = {
+    1: [4463, 127, 3, 3, 2, 31], // 316274958
+    2: [2, 3, 3, 3, 7, 11, 67, 773], // 215346978
+};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { datadogFactory } from './datadog/index.js';
 export { kvdb } from './kvdb/index.js';
 export * as ratingSvc from './rating/index.js';
 export * as commentSvc from './comment/index.js';
+export * as helper from './helper/index.js';
